623f164257a3e40432dc8a3f323c4134a474c1e4
Inno Estrera <inno.irving.estrera@gmail.com>
2016-11-17 21:54:48 -0800

    - added bluebird functionality to replace native promises. - added yield stubs for calling database

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,14 +1,17 @@
 const Router = require('koa-router');
+const Promise = require('bluebird');
+const co = require('co');
+
 const router = new Router({
     prefix: '/products'
 });
 
-router.get('/', function* (next) {
-    this.body = 'Top 20 rated products';
-});
+router.get('/', co.wrap(function* (next) {
+    this.body = yield Promise.resolve('Top 20 rated products');
+}));
 
-router.get('/:id', function* (next) {
-    this.body = 'a single product with the id ' + this.params.id;
-});
+router.get('/:id', co.wrap(function* (next) {
+    this.body = yield Promise.resolve('a single product with the id ' + this.params.id);
+}));
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
-const Boom = require('boom');
 const router = require('koa-router')();
+const Promise = require('bluebird');
+const co = require('co');
+
 const api = require('./api');
 
-router.get('/', function* (next) {
-    this.body = 'List View home';
-});
+router.get('/', co.wrap(function* (next) {
+    this.body = yield Promise.resolve('List View home');
+}));
 
 router.use(api.routes());
 
