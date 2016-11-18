9145733fb3b88a77adf80eaf66962ef4d9dfb919
Inno Estrera <inno.irving.estrera@gmail.com>
2016-11-18 02:03:25 -0800

    implemented query to get top 20 products with highest ratings from aurora db instance

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,13 +1,38 @@
 const Router = require('koa-router');
-const Promise = require('bluebird');
+const Bluebird = require('bluebird');
+const mysql = require('mysql2/promise');
 const co = require('co');
+const credentials = require('../../../credentials.json');
 
 const router = new Router({
     prefix: '/products'
 });
 
 router.get('/', co.wrap(function* (next) {
-    this.body = yield Promise.resolve('Top 20 rated products');
+    let connection = yield mysql.createConnection({
+        host: credentials.host,
+        port: credentials.port,
+        user: credentials.user,
+        password: credentials.password,
+        database: credentials.database,
+        Promise: Bluebird
+    });
+
+    let products = yield connection.query(`
+        SELECT 
+            *
+        FROM
+            product
+        ORDER BY rating DESC
+        LIMIT 20;
+    `);
+
+    // TODO make connection to S3 bucket and grab image for each product id here
+
+    connection.end();
+
+    this.body = products[0];
+
 }));
 
 router.get('/:id', co.wrap(function* (next) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
-const router = require('koa-router')();
+const Router = require('koa-router');
 const Promise = require('bluebird');
 const co = require('co');
-
 const api = require('./api');
 
+const router = new Router();
+
 router.get('/', co.wrap(function* (next) {
-    this.body = yield Promise.resolve('List View home');
+    this.redirect('/products');
 }));
 
 router.use(api.routes());
