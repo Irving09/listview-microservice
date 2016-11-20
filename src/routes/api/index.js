1bfb14d19b64b29addae8c2d9b6ab946f178bc03
Inno Estrera <inno.irving.estrera@gmail.com>
2016-11-20 10:49:27 -0800

    abstracted controller logic from routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,41 +1,13 @@
+'use strict';
+
 const Router = require('koa-router');
-const Bluebird = require('bluebird');
-const mysql = require('mysql2/promise');
-const co = require('co');
+const controller = require('../../controllers');
 
 const router = new Router({
     prefix: '/products'
 });
 
-router.get('/', co.wrap(function* (next) {
-    let connection = yield mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PW,
-        database: process.env.MYSQL_DB,
-        Promise: Bluebird
-    });
-
-    let products = yield connection.query(`
-        SELECT 
-            *
-        FROM
-            product
-        ORDER BY rating DESC
-        LIMIT 20;
-    `);
-
-    // TODO make connection to S3 bucket and grab image for each product id here
-
-    connection.end();
-
-    this.body = products[0];
-
-}));
-
-router.get('/:id', co.wrap(function* (next) {
-    this.body = yield Promise.resolve('a single product with the id ' + this.params.id);
-}));
+router.get('/', controller.getProducts);
+router.get('/:id', controller.getProductById);
 
 module.exports = router;
